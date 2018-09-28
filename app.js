c2c1c9013a6ba876cfb82b81cdf2bcb81bc0eb9a
Raiyan <raiyan.quaium@carleton.ca>
2018-09-28 10:59:00 -0400

    Displayed input from form

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 
 
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
 
-
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 // Uses the ejs templating engine
 app.set('view engine', 'ejs');
 app.use('/assets',express.static('assets'));
 
-app.get('/',function(req,res){  
-    res.render('index');
+app.get('/',function(req,res){
+    res.render('index', {qs: req.query});
+});
+
+app.post('/info', urlencodedParser, function(req,res){
+    res.render('info', {data: req.body});
 });
 //Got to localhost:3000
 app.listen(3000);
