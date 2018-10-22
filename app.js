5bbf1248bf474ee3f4c3767b5dc2d4818fc160c3
Raiyan <raiyan.quaium@carleton.ca>
2018-10-22 19:18:43 -0400

    Added css style, fixed up small bugs

--- app.js
+++ app.js
@@ -11,9 +11,7 @@ var app = express();
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 db.serialize(function() {
- // db.run("CREATE TABLE books (TITLE VARCHAR(255), BOOK VARCHAR(255), PAGE VARCHAR(255), KEY INT)");
-
-
+  db.run("CREATE TABLE IF NOT EXISTS books (TITLE VARCHAR(255), BOOK VARCHAR(255), PAGE VARCHAR(255), KEY INT PRIMARY KEY)");
 });
 
 //db.close();
@@ -28,13 +26,12 @@ app.get('/',function(req,res){
 
 app.post('/info', urlencodedParser, function(req,res){
 	let db = new sqlite3.Database('books.db');
-	var stmt = db.prepare("INSERT INTO books VALUES (?,?,?,?)");
-	stmt.run(req.body.title, req.body.book, req.body.page, req.body.key);
-	stmt.finalize();
-		
-	/*db.each("SELECT TITLE, BOOK, PAGE, KEY FROM books", function(err, row) {
-      console.log(row.TITLE + row.BOOK + row.PAGE + row.KEY);
-	});*/
+	let data = [req.body.title, req.body.book, req.body.page, req.body.key]
+	let sql = 'INSERT INTO books VALUES (?,?,?,?)';
+	db.run(sql, data, function(err) {
+	if (err) {
+		return console.error(err.message);
+	}});
     
     res.render('info', {data: req.body});
 	db.close();
@@ -44,34 +41,26 @@ app.get('/update', urlencodedParser, function(req,res){
 	let db = new sqlite3.Database('books.db');
 	let data = [req.body.title, req.body.book, req.body.page, req.body.key, req.body.key]
 	let sql = 'UPDATE books set TITLE = ?, BOOK = ?, PAGE = ?, KEY = ? WHERE KEY = ?';
+					
 	db.run(sql, data, function(err) {
 	if (err) {
 		return console.error(err.message);
 	}});
-	//var stmt = db.prepare("UPDATE books set TITLE = ?, BOOK = ?, PAGE = ?, KEY = ? WHERE KEY = ?");
-	//stmt.run(req.body.title, req.body.book, req.body.page, req.body.key, req.body.key);
-	//stmt.finalize();
-		
-	/*db.each("SELECT TITLE, BOOK, PAGE, KEY FROM books", function(err, row) {
-      console.log(row.TITLE + row.BOOK + row.PAGE + row.KEY);
-	});*/
-    
 	
+	console.log(`Row(s) updated: ${this.changes}`);
     res.render('update', {qs: req.query});
 	db.close();
 });
 
 app.get('/delete', urlencodedParser, function(req,res){
 	let db = new sqlite3.Database('books.db');
-	var stmt2 = db.prepare("DELETE FROM books WHERE key = ?");
-	stmt2.run(req.body.key);
-	stmt2.finalize();
-		
-	/*db.each("SELECT TITLE, BOOK, PAGE, KEY FROM books", function(err, row) {
-      console.log(row.TITLE + row.BOOK + row.PAGE + row.KEY);
-	});*/
-    
-	
+	let data = [req.body.key]
+	let sql = 'DELETE FROM books WHERE key = ?';
+	db.run(sql, data, function(err) {
+	if (err) {
+		return console.error(err.message);
+	}});
+	console.log(`Row(s) deleted ${this.changes}`);
     res.render('delete', {qs: req.query});
 	db.close();
 });
@@ -80,21 +69,19 @@ app.get('/delete', urlencodedParser, function(req,res){
 app.get('/books',function(req,res){
 	    let db = new sqlite3.Database('books.db');
         res.write('<h2>' +  'BOOKS' + '</h2>');
-		
-	    /*db.each("SELECT TITLE, BOOK, PAGE, KEY FROM books", function(err, row) {
-		   //res.write('<p>' + row.title + ": " + row.book + ": " + row.page + ": " + row.key + '</p>');
-		   console.log(row.TITLE + row.BOOK + row.PAGE + row.KEY);
-		   res.write('<p>' + row.TITLE + ": " + row.BOOK + ": " + row.PAGE + ": " + row.KEY + '</p>');
-		});*/
 	
 		db.all("SELECT TITLE, BOOK, PAGE, KEY FROM books", function(err, rows){
 		  for(var i=0; i<rows.length; i++){
 			  console.log(i + "TITLE: " + rows[i].TITLE + "BOOK: " + rows[i].BOOK + "PAGE: " + rows[i].PAGE + "KEY: " + rows[i].KEY);
 			  res.write('<p>' + rows[i].TITLE + ": " + rows[i].BOOK + ": " + rows[i].PAGE + ": " + rows[i].KEY + '</p>');
 		  }	  
+		//need this last line or it hangs because of something about res.write works as an asynchronosu function
+		res.end() 
 		});
 		
-		db.close()
+		db.close();
+
+		
 });
 
 //Go to localhost:3000
